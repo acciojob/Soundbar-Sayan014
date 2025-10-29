75cf8d23720854b407e8a4004f593e0fd011533d
Sayan014 <90103006+Sayan014@users.noreply.github.com>
2025-10-29 18:38:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+document.querySelectorAll('.btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    stopAllSounds();
+    const audio = new Audio(`sounds/${btn.innerText}.mp3`);
+    audio.play();
+  });
+});
+
+document.querySelector('.stop').addEventListener('click', stopAllSounds);
+
+function stopAllSounds() {
+  sounds.forEach(sound => {
+    const audio = new Audio(`sounds/${sound}.mp3`);
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
